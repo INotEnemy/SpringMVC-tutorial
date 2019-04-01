005a559b41100e4c1db25c313594c05bb563fc22
Wo_land <vladiok2@mail.ru>
2019-04-01 15:52:58 +0300

    #1 fix mistake

--- src/main/java/ru/sevsu/rest/configuration/MappingConfiguration.java
+++ src/main/java/ru/sevsu/rest/configuration/MappingConfiguration.java
@@ -2,18 +2,12 @@ package ru.sevsu.rest.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.sevsu.rest.mapping.PlainMapping;
 import ru.sevsu.rest.mapping.StudentMapping;
 import ru.sevsu.rest.mapping.TableMapping;
 
 @Configuration
 public class MappingConfiguration {
-
-    @Bean
-    public PlainMapping getPlainMapping() {
-        return new PlainMapping();
-    }
-
+    
     @Bean
     public TableMapping getTableMapping() {
         return new TableMapping();
